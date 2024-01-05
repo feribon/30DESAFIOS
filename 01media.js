eabac407e96eb8d08a5abeac189354a4f7058f7e
Felipe Grassi Ribon <felipe.gribon@hotmail.com>
2024-01-05 10:01:47 -0300

    primeiro desafio

---
+++ 01media.js
@@ -0,0 +1,11 @@
+function media(...dados) {
+  let soma = 0;
+  for (let i in dados) {
+    soma += dados[i];
+  }
+  return soma / dados.length;
+}
+console.log(media(10, 9, 6, 8, 9, 1, 5, 7));
+console.log(media(2, 5, 7, 1, -2));
+console.log(media(10, 10, 10, 10, 9));
+console.log(media(25, 75));
