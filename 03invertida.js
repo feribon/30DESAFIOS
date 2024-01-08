35a276eacfa268a728538039757b2fdf9b318225
Felipe Grassi Ribon <felipe.gribon@hotmail.com>
2024-01-07 21:33:48 -0300

    desafio 03

---
+++ 03invertida.js
@@ -0,0 +1,12 @@
+function reverse(arr) {
+  let invertido = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    invertido.push(arr[i])
+  }
+  return invertido;
+}
+console.log(reverse([0, 9, 6, 8, 9, 1, 5, 7]));
+console.log(reverse(["Oh", "Hi", "Mark"]));
+console.log(reverse([false, true, true, true]));
+console.log(reverse(["It's", "not", true, 0]));
+
