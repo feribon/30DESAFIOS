3433b533e51dafbc594217d5d9ca4f60fb906de4
Felipe Grassi Ribon <felipe.gribon@hotmail.com>
2024-01-15 22:44:23 -0300

    desafio 09

---
+++ 09validString.js
@@ -0,0 +1,31 @@
+//verificar se a string contem as mesmas quantidades de cada caracter
+function entrada(str) {
+  let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    if (!obj[str[i]]) {
+      obj[str.charAt(i)] = 1;
+    } else {
+      obj[str.charAt(i)]++;
+    }
+  }
+  return todosIguais(Object.values(obj));
+}
+
+function todosIguais(array) {
+  if (array.length === 0) {
+    return true;
+  }
+  let primeiroElemento = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] !== primeiroElemento) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(entrada("This is Thee"));
+console.log(entrada("ssd"));
+console.log(entrada(""));
+console.log(entrada("Lorem ipsum"));
+console.log(entrada("QQwweerrttyy"));
