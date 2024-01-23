7b1a2456da6e625bf75f1bcd49a219693918fe54
Felipe Grassi Ribon <60076271+feribon@users.noreply.github.com>
2024-01-23 08:30:16 -0300

    Update 15romanos1.js

--- 15romanos1.js
+++ 15romanos1.js
@@ -3,10 +3,8 @@ function entrada(dados) {
   let obj = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
   let arr = dados.split("");
   let arabico = arr.map((item) => obj[item]);
-  let soma = 0;
 
   return arabico.reduceRight((acc, atual, idx, arr) => {
-    console.log(`atual: ${atual} arr[idx+1]:${arr[idx+1]}`)
     if (atual < arr[idx + 1]) {
       return acc - atual;
     } else {
