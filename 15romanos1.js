f0da71606ae87eabffc3424139f72b4326a33b11
Felipe Grassi Ribon <felipe.gribon@hotmail.com>
2024-01-24 23:49:37 -0300

    15 correcao

--- 15romanos1.js
+++ 15romanos1.js
@@ -1,4 +1,4 @@
-// I1, V5, X10, L50, C100, D500, M1000
+// I1, V5, X10, L50, C100, D500, M1000, converta os numeros romanos para arabico
 function entrada(dados) {
   let obj = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
   let arr = dados.split("");
